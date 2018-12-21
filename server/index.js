9b3c32a2f1e25f1216ce0e0a0d4d0212f5c7422a
Aaron Deane <aaronmdeane@gmail.com>
2018-12-21 10:58:03 -0800

    Updated script and created server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// Setup express and define port
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const Photos = require('../db/index');
+
+const app = express();
+const PORT = 3001;
+
+
+// Serve up middleware
+app.use(express.static('../client/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+// Route to return all records
+app.get('/api/watches/:wid/photos', (req, res, next) => {
+  const watchId = req.params.wid;
+  if (watchId === undefined) {
+    res.status(404).end();
+  } else {
+    Photos.getPhotosById(watchId, (err, results) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).end(results);
+      }
+    });
+  }
+});
+
+app.listen(PORT, () => console.log(`Express is listening on port ${PORT}!`));
