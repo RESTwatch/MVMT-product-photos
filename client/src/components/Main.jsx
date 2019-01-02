64643738c85a130d3a9ea32572e1c02531a2a178
Aaron Deane <aaronmdeane@gmail.com>
2019-01-01 19:35:17 -0800

    Added function to pan image when zoomed in and added styleloader

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Main = (props) => (
   <div className="photos-current-img" onClick={props.toggleZoom}>
     <img className="photos-slider-image" src={props.currImage}/>
+    <img className="photos-zoom-icon" src="https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/zoom-hover.png" />
   </div>
 )
 
--- client/src/components/Zoom.jsx
+++ client/src/components/Zoom.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 const Zoom = (props) => {
   let showZoom = props.zoomed ? 'photos-zoom-box-true' : 'photos-zoom-box-false'
 
+  const panImage = (e) => {
+    let left = -e.clientX / 4;
+    let top = -e.clientY;
+    const divStyle = {
+      position: 'absolute',
+      top: top,
+      left: left,
+    };
+    console.log(divStyle);
+    return divStyle;
+  }
+
   return (
-    <div className={showZoom} onClick={props.toggleZoom}>
-      <img className="photos-zoom-image" src={props.currImage}/>
-      <div className="photos-zoom-icon">
-        <img className="photos-zoom-hover-icon" src="https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/zoom-hover.jpg"></img>
-      </div>
+    <div className={showZoom} onClick={props.toggleZoom} onMouseMove={panImage}>
+      <img className="photos-zoom-image" src={props.currImage} />
     </div>
   );
 }
