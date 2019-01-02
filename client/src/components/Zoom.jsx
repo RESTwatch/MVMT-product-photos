4e596593882ff7f5f1a7e70ce4559411255cb5ca
Aaron Deane <aaronmdeane@gmail.com>
2019-01-02 10:58:53 -0800

    Implemented pan feature on zoomed image

--- client/src/components/Zoom.jsx
+++ client/src/components/Zoom.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 
-const Zoom = (props) => {
-  let showZoom = props.zoomed ? 'photos-zoom-box-true' : 'photos-zoom-box-false'
+class Zoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      top: 0,
+      left: 0,
+    }
+    this.panImage = this.panImage.bind(this);
+  }
+  
+  panImage (e) {
+    let xPos = -e.clientX / 4;
+    let yPos = -e.clientY;
 
-  const panImage = (e) => {
-    let left = -e.clientX / 4;
-    let top = -e.clientY;
-    const divStyle = {
-      position: 'absolute',
-      top: top,
-      left: left,
-    };
-    console.log(divStyle);
-    return divStyle;
+    this.setState({
+      top: yPos,
+      left: xPos,
+    });
   }
 
-  return (
-    <div className={showZoom} onClick={props.toggleZoom} onMouseMove={panImage}>
-      <img className="photos-zoom-image" src={props.currImage} />
-    </div>
-  );
+  render () {
+    let showZoom = this.props.zoomed ? 'photos-zoom-box-true' : 'photos-zoom-box-false'
+    let divStyle = {
+      position: 'absolute',
+      top: this.state.top,
+      left: this.state.left,
+    }
+
+    return (
+      <div className={showZoom} onClick={this.props.toggleZoom} onMouseMove={this.panImage}>
+        <img className="photos-zoom-image" src={this.props.currImage} style={divStyle}/>
+      </div>
+    )
+  }
+  
 }
 
 export default Zoom;
