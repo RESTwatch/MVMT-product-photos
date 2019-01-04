d2f0b40ab1d9cdb457d7076c5e4524fb624e5547
Aaron Deane <aaronmdeane@gmail.com>
2019-01-03 17:53:05 -0800

    Completed refactor to serve up app using Docker

--- data_generation/data_script.js
+++ data_generation/data_script.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const { Photos } = require('../db/schema.js');
 
-mongoose.connect('mongodb://localhost/photos', { useNewUrlParser: true });
+mongoose.connect('mongodb://172.17.0.2/photos', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/photos', { useNewUrlParser: true });
 
 
 const dataScript = (min, max) => {
@@ -15,10 +16,10 @@ const dataScript = (min, max) => {
       backImg: 'https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/100_back.jpg',
       box: 'https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/box.jpg',
       styleImg: 'https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/100_style.jpg',
-    },
+    }
   );
 
-  for (let i = min; i < max; i += 1) {
+  for (let i = min; i <= max; i += 1) {
     const randomPhotoNum = () => {
       const low = 100;
       const high = 120;
@@ -33,7 +34,7 @@ const dataScript = (min, max) => {
         backImg: `https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/${randomPhotoNum()}_back.jpg`,
         box: 'https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/box.jpg',
         styleImg: `https://s3-us-west-1.amazonaws.com/hrsf107-the-event-handlers-mvmt-photos/${randomPhotoNum()}_style.jpg`,
-      },
+      }
     );
   }
   return watchPhotos;
@@ -41,8 +42,11 @@ const dataScript = (min, max) => {
 
 const data = dataScript(101, 199);
 
-data.forEach((record) => {
+data.forEach((record, i) => {
   Photos.create(record, (err) => {
     if (err) throw err;
+    if (i === 99) {
+      process.exit();
+    }
   });
 });
--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Photos } = require('./schema.js');
 
-mongoose.connect('mongodb://localhost/photos', { useNewUrlParser: true });
+mongoose.connect('mongodb://172.17.0.2/photos', { useNewUrlParser: true });
 
 const getPhotosById = (watchId, serverCB) => {
   Photos.findOne({ _id: Number(watchId) }, serverCB );
