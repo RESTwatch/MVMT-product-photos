9d8ade4bc0a555a43f5413786103249dcd20c3a2
Megan HR <meganhr@Megans-MacBook.local>
2019-01-18 11:38:44 -0800

    Added redis cache to get request

---
+++ db/redis/index.js
@@ -0,0 +1,35 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: '6379',
+});
+
+client.on('ready', () => {
+  console.log('Redis connected');
+});
+
+const getRedis = (id, callback) => {
+  client.get(id, (err, reply) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, JSON.parse(reply));
+    }
+  });
+};
+
+const setRedis = (id, data, callback) => {
+  client.set(id, JSON.stringify(data), 'EX', 6000, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      getRedis(id, callback);
+    }
+  });
+};
+
+module.exports = {
+  getRedis,
+  setRedis,
+};
