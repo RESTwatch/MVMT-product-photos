dc3ec1fd1a9316bf445e40a6e89f5d44d676d3ff
Megan HR <meganhr@Megans-MacBook.local>
2019-01-22 19:42:25 -0800

    Added minor changes for deployment

--- pg_stresstest.js
+++ pg_stresstest.js
@@ -2,15 +2,19 @@ import http from 'k6/http';
 import { sleep } from "k6";
 
 export let options = {
-  duration: "5m",
+  duration: "1h",
   vus: 50,
   rps: 1000,
 }
 
 export default function () {
-  const low = 8000000;
-  const high = 10000000;
-  const num = Math.floor(Math.random() * (high - low + 1)) + low;
+  const randomNum = Math.random();
+  let low = 100;
+  const high = 10000100;
+  if (randomNum > 0.2) {
+    low = 9009100;
+  }
+  const num = Math.floor(randomNum * (high - low + 1)) + low;
   http.get(`http://localhost:3001/api/watches/${num}/photos`);
   sleep(1);
 }
